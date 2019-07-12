0879c6e687ce01e5ed0c12558c69d79a549a8ea8
Any <Any@ANY>
2019-07-12 19:28:11 +0400

    Add project files.

---
+++ BooksWithAdoNet/SqlManeger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BooksWithAdoNet
+{
+    public static class SqlManeger
+    {
+        private const string connString = @"Data Source=(local);Initial Catalog=BookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Pooling=true";
+        public static DataTable Execute(string query)
+        {
+            var ds = new DataTable();
+
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                using (var adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand(query, connection);
+                    adapter.Fill(ds);
+                }
+            }
+            return ds;
+        }
+    }
+}
