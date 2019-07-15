d655d956fd797358fa91c593af1472de8392845c
Any <Any@ANY>
2019-07-15 21:43:50 +0400

    Add Universal DB functions

--- BooksWithAdoNet/MainWindow.xaml.cs
+++ BooksWithAdoNet/MainWindow.xaml.cs
@@ -5,42 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace BooksWithAdoNet
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-        private void FullView_Click(object sender, RoutedEventArgs e)
+        public string ConnectionString()
+            {
+                SqlConnectionStringBuilder connString = new SqlConnectionStringBuilder();
+                connString.DataSource = $"{ServerNameTextBox.Text}";
+                connString.InitialCatalog = $"{DataBaseNameTextBox.Text}";
+                connString.IntegratedSecurity = true;
+                connString.Pooling = true;
+            return connString.ToString();
+            }
+    private void FullView_Click(object sender, RoutedEventArgs e)
         {
+            string connString = ConnectionString();
             ClassicView1.IsChecked = false;
-            DataTable dt = SqlManeger.Execute("Select * FROM FullView");
+            DataTable dt = SqlManeger.Execute("Select * FROM FullView",connString);
             DataGrid.ItemsSource = dt.DefaultView;
         }
         private void ClassicView_Click(object sender, RoutedEventArgs e)
         {
             FullView1.IsChecked = false;
-            DataTable dt = SqlManeger.Execute("Select * FROM ClassicView");
+            string connString = ConnectionString();
+            DataTable dt = SqlManeger.Execute("Select * FROM ClassicView",connString);
             DataGrid.ItemsSource = dt.DefaultView;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             FullView1.IsChecked = false;
             ClassicView1.IsChecked = false;
+            string connString = ConnectionString();
             string query = $"{ComboBox1.Text} {TextBox.Text} {TextBlock.Text} {TextBox2.Text};";
-            DataTable dt = SqlManeger.Execute(query);
+            DataTable dt = SqlManeger.Execute(query,connString);
             DataGrid.ItemsSource = dt.DefaultView;
         }
         private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -64,7 +66,15 @@ namespace BooksWithAdoNet
                     TextBlock.Text = "WHERE";
                     TextBox2.Text = "";
                     break;
-            }    
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string connString = ConnectionString();
+            SqlManeger.Execute(connString);
+            DataGrid.Columns.Clear();
+            DataGrid.ItemsSource = null;
         }
     }
 }
+
--- BooksWithAdoNet/SqlManeger.cs
+++ BooksWithAdoNet/SqlManeger.cs
@@ -1,38 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BooksWithAdoNet;
+using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace BooksWithAdoNet
-{
     public static class SqlManeger
     {
-        private const string connString = @"Data Source=(local);Initial Catalog=BookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Pooling=true";
-        public static DataTable Execute(string query)
-        {
-            var ds = new DataTable();
+    //private const string connString = @"Data Source=(local);Initial Catalog=BookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Pooling=true";
+    // private const string connString = @"Data Source=(local);Initial Catalog=BookDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Pooling=true";
 
-            using (var connection = new SqlConnection(connString))
+    public static DataTable Execute(string query, string connString)
+    {
+        var ds = new DataTable();
+
+        using (var connection = new SqlConnection(connString))
+        {
+            connection.Open();
+            using (var adapter = new SqlDataAdapter())
             {
-                connection.Open();
-                using (var adapter = new SqlDataAdapter())
+                adapter.SelectCommand = new SqlCommand(query, connection);
+                try
                 {
-                    adapter.SelectCommand = new SqlCommand(query, connection);
-                    try
-                    {
-                         adapter.Fill(ds);
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
+                    adapter.Fill(ds);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
                 }
             }
-            return ds;
         }
+        return ds;
+    }
+    public static void Execute(string connString)
+    {
+        using (SqlConnection connection = new SqlConnection(connString))
+            try
+            {
+                connection.Open();
+                if (connString.Length != 67)
+                MessageBox.Show($"You connect to {connection.Database.ToString()}");
+                else
+                    MessageBox.Show("Please select your DataSource and DataName");
+
+            }
+            catch (Exception ex)
+            {
+            MessageBox.Show(ex.Message);
+            }    
     }
 }
+
